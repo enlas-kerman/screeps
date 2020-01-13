4906db4e5204c4ee7ee6aa6d0850db5293d95421
enlas-kerman <enlas.kerman@gmail.com>
2020-01-12 21:57:58 -0500

    Improved harvester encapsulation

--- src/main.js
+++ src/main.js
@@ -1,32 +1,30 @@
+let UpgraderRole = require('roles_upgrader');
+
+
+let MAX_UPGRADERS = 5;
+let UPGRADER_CONFIG = [WORK, CARRY, MOVE];
+
 
 module.exports.loop = () => {
     
     let mara = Game.spawns['MARA'];
+    let ruins = mara.room.find(FIND_RUINS);
+
+    let numberOfCreeps = Object.keys(Game.creeps).length;
+    if (numberOfCreeps < MAX_UPGRADERS) {
+        if (mara.isActive() && mara.spawning == null) {
+            if (typeof Memory.lastUpgraderIndex === 'undefined') {
+                Memory.lastUpgraderIndex = 5;
+            }
+            Memory.lastUpgraderIndex++;
+            mara.spawnCreep(UPGRADER_CONFIG, 'H' + Memory.lastUpgraderIndex);
+        }
+    }
 
     for (let name in Game.creeps) {
         let creep = Game.creeps[name];
-        let sources = creep.room.find(FIND_SOURCES);
-        if (creep.store.getFreeCapacity() > 0 && creep.harvest(sources[0]) != ERR_NOT_IN_RANGE) {
-
-        } else {
-            if (creep.store.getFreeCapacity() == creep.store.getCapacity()) {
-                creep.moveTo(sources[0]);
-            } else {
-
-                if (mara.energy < mara.energyCapacity) {
-                    if (creep.transfer(mara, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(mara);
-                    }
-                } else {
-                    if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(creep.room.controller);
-                    }
-                }
-
-            }
-
-        }
-
+        let upgrader = new UpgraderRole.Upgrader(creep);
+        upgrader.update(mara, ruins);
     }
     
 }
